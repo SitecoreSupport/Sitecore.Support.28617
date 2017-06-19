42db10cac102e43fbca1f63735cb46d5b68272ea
ArtemNizelnyk <arn@sitecore.net>
2017-06-19 08:45:05 +0300

    Initial Fix

---
+++ src/Sitecore.Support.28617/Commerce/Engine/Connect/Pipelines/Orders/FixedGetVisitorOrders.cs
@@ -0,0 +1,170 @@
+﻿namespace Sitecore.Support.Commerce.Engine.Connect.Pipelines.Orders
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Globalization;
+  using System.Linq;
+
+  using Sitecore.Commerce.Connect.CommerceServer.Orders.Models;
+  using Sitecore.Commerce.ServiceProxy;
+  using Sitecore.Commerce.Core;
+  using Sitecore.Commerce.Entities;
+  using Sitecore.Commerce.Pipelines;
+  using Sitecore.Commerce.Plugin.Orders;
+  using Sitecore.Commerce.Services.Orders;
+  using Sitecore.Diagnostics;
+  using Microsoft.OData.Client;
+  using Sitecore.Commerce.Engine.Connect.Pipelines;
+
+  /// <summary>
+  /// Pipeline processor to get visitor's orders
+  /// </summary>
+  /// <seealso cref="Sitecore.Commerce.Engine.Connect.Pipelines.PipelineProcessor" />
+  public class FixedGetVisitorOrders : PipelineProcessor
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetVisitorOrders"/> class.
+    /// </summary>
+    /// <param name="entityFactory">The entity factory.</param>
+    /// <param name="itemsToTake">The items to take</param>
+    public FixedGetVisitorOrders([NotNull]IEntityFactory entityFactory, string itemsToTake)
+    {
+      Assert.ArgumentNotNull(entityFactory, "entityFactory");
+
+      this.EntityFactory = entityFactory;
+      this.ItemsToTake = Convert.ToInt32(itemsToTake, CultureInfo.InvariantCulture);
+    }
+
+    /// <summary>
+    /// Gets the entity factory.
+    /// </summary>
+    /// <value>
+    /// The entity factory.
+    /// </value>
+    public IEntityFactory EntityFactory { get; private set; }
+
+    /// <summary>
+    /// Gets the items to take.
+    /// </summary>
+    /// <value>
+    /// The items to take.
+    /// </value>
+    public int ItemsToTake { get; private set; }
+
+    /// <summary>
+    /// Process the Pipeline event
+    /// </summary>
+    /// <param name="args">The arguments.</param>
+    public override void Process(ServicePipelineArgs args)
+    {
+      GetVisitorOrdersRequest request;
+      GetVisitorOrdersResult result;
+      FixedPipelineUtility.ValidateArguments(args, out request, out result);
+
+      try
+      {
+        Assert.IsNotNull(request.CustomerID, "request.CustomerID");
+        Assert.IsNotNull(request.Shop.Name, "request.Shop.Name");
+
+        var orderHeaders = new List<CommerceOrderHeader>();
+
+        var container = this.GetContainer(request.Shop.Name, request.CustomerID);
+
+        var skip = 0;
+        var ordersList = Sitecore.Commerce.ServiceProxy.Proxy.GetValue(
+          container.GetList(
+            string.Format(CultureInfo.InvariantCulture, "Orders-ByCustomer-{0}", request.CustomerID),
+            "Sitecore.Commerce.Plugin.Orders.Order, Sitecore.Commerce.Plugin.Orders",
+            skip,
+            this.ItemsToTake).Expand("Items($expand=Components)"));
+
+        orderHeaders.AddRange(this.TranslateOrderHeaders(ordersList.Items));
+
+        while (skip < ordersList.TotalItemCount)
+        {
+          skip += this.ItemsToTake;
+          ordersList = Sitecore.Commerce.ServiceProxy.Proxy.GetValue(
+            container.GetList(
+              string.Format(CultureInfo.InvariantCulture, "Orders-ByCustomer-{0}", request.CustomerID),
+              "Sitecore.Commerce.Plugin.Orders.Order, Sitecore.Commerce.Plugin.Orders",
+              skip,
+              this.ItemsToTake).Expand("Items($expand=Components)"));
+          if (ordersList == null || !ordersList.Items.Any())
+          {
+            continue;
+          }
+
+          orderHeaders.AddRange(this.TranslateOrderHeaders(ordersList.Items));
+        }
+
+        result.OrderHeaders = orderHeaders;
+      }
+      catch (ArgumentException e)
+      {
+        result.Success = false;
+        result.SystemMessages.Add(FixedPipelineUtility.CreateSystemMessage(e));
+      }
+      catch (DataServiceQueryException aggregateException)
+      {
+        result.Success = false;
+        result.SystemMessages.Add(FixedPipelineUtility.CreateSystemMessage(aggregateException.Message));
+      }
+
+      base.Process(args);
+    }
+
+    /// <summary>
+    /// Translate a list of <see cref="Order"/> to a list of <see cref="CommerceOrderHeader"/>.
+    /// </summary>
+    /// <param name="orders">the list of <see cref="Order"/>.</param>
+    /// <returns>A list of <see cref="CommerceOrderHeader"/>.</returns>
+    public IEnumerable<CommerceOrderHeader> TranslateOrderHeaders(IEnumerable<CommerceEntity> orders)
+    {
+      Assert.ArgumentNotNull(orders, "orders");
+
+      var orderHeaders = new List<CommerceOrderHeader>();
+      var commerceEntities = orders as CommerceEntity[] ?? orders.ToArray();
+      if (!commerceEntities.Any())
+      {
+        return orderHeaders;
+      }
+
+      foreach (var order in commerceEntities.Cast<Order>())
+      {
+        var orderHeader = this.EntityFactory.Create<CommerceOrderHeader>("OrderHeader");
+
+        orderHeader.ExternalId = order.Id;
+        orderHeader.ShopName = order.ShopName;
+        orderHeader.Name = order.Name;
+        orderHeader.OrderID = order.OrderConfirmationId;
+        orderHeader.TrackingNumber = order.OrderConfirmationId;
+        orderHeader.Status = order.Status;
+        orderHeader.OrderDate = order.OrderPlacedDate.DateTime;
+        if (order.DateCreated.HasValue)
+        {
+          orderHeader.Created = order.DateCreated.Value.DateTime;
+        }
+
+        if (order.DateUpdated.HasValue)
+        {
+          orderHeader.LastModified = order.DateUpdated.Value.DateTime;
+        }
+
+        if (order.Components != null && order.Components.Any())
+        {
+          var contactComponent = order.Components.OfType<ContactComponent>().FirstOrDefault();
+          if (contactComponent != null)
+          {
+            orderHeader.Email = contactComponent.Email ?? string.Empty;
+            orderHeader.UserId = contactComponent.ShopperId;
+            orderHeader.CustomerId = contactComponent.CustomerId ?? contactComponent.ShopperId;
+          }
+        }
+
+        orderHeaders.Add(orderHeader);
+      }
+
+      return orderHeaders;
+    }
+  }
+}
---
+++ src/Sitecore.Support.28617/Commerce/Engine/Connect/Pipelines/Orders/FixedTranslateOrderToEntity.cs
@@ -0,0 +1,83 @@
+﻿namespace Sitecore.Support.Commerce.Engine.Connect.Pipelines.Orders
+{
+  using Sitecore.Commerce.Connect.CommerceServer.Orders.Models;
+  using Sitecore.Commerce.Engine.Connect.Pipelines.Arguments;
+  using Sitecore.Commerce.Engine.Connect.Pipelines.Orders;
+  using Sitecore.Commerce.Entities;
+  using Sitecore.Commerce.Plugin.Orders;
+  using System.Collections.Generic;
+  using System.Collections.ObjectModel;
+  using System.Linq;
+
+  /// <summary>
+  ///  Pipeline processor to translate from a <see cref="Order" /> to a <see cref="CommerceOrder" />.
+  /// </summary>
+  /// <seealso>
+  ///     <cref>
+  ///         Sitecore.Commerce.Engine.Connect.Pipelines.TranslateODataEntityToEntity{Sitecore.Commerce.Engine.Connect.Pipelines.Arguments.TranslateOrderToEntityRequest,
+  ///         Sitecore.Commerce.Engine.Connect.Pipelines.Arguments.TranslateOrderToEntityResult,
+  ///         Sitecore.Commerce.Plugin.Orders.Order, Sitecore.Commerce.Connect.CommerceServer.Orders.Models.CommerceOrder}
+  ///     </cref>
+  /// </seealso>
+  public class FixedTranslateOrderToEntity : TranslateOrderToEntity
+  {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TranslateOrderToEntity"/> class.
+    /// </summary>
+    /// <param name="entityFactory">The entity factory.</param>
+    public FixedTranslateOrderToEntity([NotNull] IEntityFactory entityFactory)
+          : base(entityFactory)
+    {
+    }
+
+    /// <summary>
+    /// Translates a <see cref="Order" /> to a specific <see cref="CommerceOrder" />
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="source">The  <see cref="Sitecore.Commerce.Plugin.Orders.Order" /></param>
+    /// <param name="destination">The <see cref="CommerceOrder" /> to copy to</param>
+    protected override void Translate(TranslateOrderToEntityRequest request, Sitecore.Commerce.Plugin.Orders.Order source, CommerceOrder destination)
+    {
+      base.Translate(request, source, destination);
+
+      destination.ExternalId = source.Id;
+      destination.ShopName = source.ShopName;
+      destination.Name = source.Name;
+      destination.LineItemCount = source.Lines.Count;
+      destination.OrderID = source.Id;
+      destination.TrackingNumber = source.OrderConfirmationId;
+      destination.Status = source.Status;
+      destination.OrderDate = source.OrderPlacedDate.DateTime;
+      if (source.DateCreated.HasValue)
+      {
+        destination.Created = source.DateCreated.Value.DateTime;
+      }
+
+      if (source.DateUpdated.HasValue)
+      {
+        destination.LastModified = source.DateUpdated.Value.DateTime;
+      }
+
+      if (source.Components != null && source.Components.Any())
+      {
+        var contactComponent = source.Components.OfType<Sitecore.Commerce.Core.ContactComponent>().FirstOrDefault();
+        if (contactComponent != null)
+        {
+          destination.Email = contactComponent.Email ?? string.Empty;
+          destination.UserId = contactComponent.ShopperId;
+          destination.CustomerId = contactComponent.CustomerId ?? contactComponent.ShopperId;
+        }
+      }
+
+      destination.OrderForms = new ReadOnlyCollection<CommerceOrderForm>(new List<CommerceOrderForm> { new CommerceOrderForm() });
+
+      base.TranslateLines(request, source, destination);
+      base.TranslateAdjustments(request, source, destination);
+      base.TranslateShippingInfo(request, source, destination);
+      base.TranslatePaymentInfo(request, source, destination);
+      base.TranslateTotals(request, source, destination);
+    }
+
+  }
+}
+
